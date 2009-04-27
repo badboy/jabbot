a2e2392ac6c6bead1cfeb29679a892423446d194
BadBoy_ <psp.badboy@gmail.com>
2009-04-27 12:47:29 +0200

    damn...i forgot to bump Jabbot::VERSION to 0.1.2

--- lib/jabbot.rb
+++ lib/jabbot.rb
@@ -9,7 +9,7 @@ require File.join(File.dirname(__FILE__), 'hash')
 module Jabbot
 
   # :stopdoc:
-  VERSION = '0.0.1'
+  VERSION = '0.1.2'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
