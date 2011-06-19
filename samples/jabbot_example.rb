87ee743962884266f42f1c54ec630e30d4aefed2
Jan-Erik Rediger <badboy@archlinux.us>
2011-06-19 18:18:23 +0200

    better documented example.

--- samples/jabbot_example.rb
+++ samples/jabbot_example.rb
@@ -10,34 +10,39 @@ configure do |conf|
   conf.password = 'secret'
 end
 
+# Just print all incoming messages to stdout.
 message do |message, params|
   puts message
 end
 
+# Agree to certain users, no matter what they said.
 message :from => [:abcd, :efgh] do |message, params|
   post "I agree!" => message.user
 end
 
-query /\A.quit\Z/, :from => :admin do |message, params|
-  post "good bye! I'm going to sleep"
+# The user 'admin' can quit the bot via private message.
+query /\A!quit\Z/, :from => :admin do |message, params|
+  post "good bye! I'm going to sleep" => message.user
   close
 end
 
-message ".answer :me" do |message, params|
-  post "ok, the answer is: #{params[:me]}"
-end
-
-message :exact => "!exit" do |message, params|
+# Same as query above, but for all users and global messages.
+message :exact => "!quit" do |message, params|
   post "Bye Bye!"
   close
 end
 
+# Respond with whatever was given as the answer.
+message ".answer :me" do |message, params|
+  post "ok, the answer is: #{params[:me]}"
+end
+
 ## You need a extern Google engine
-## write your own or search github.com / rubyforge.org
-#message /\A\.google (.+)/im do |message, params|
-#  search_result = MyGoogleSearch.lookup(params.first)
-#  post "Google Search for '#{params.first}':\n#{search_result}"
-#end
+## write your own or search github.com
+message /\A!google (.+)/im do |message, params|
+  search_result = MyGoogleSearch.lookup(params.first)
+  post "Google Search for '#{params.first}':\n#{search_result}"
+end
 
 leave do |message, params|
   post "and there he goes...good bye, #{message.user}"
